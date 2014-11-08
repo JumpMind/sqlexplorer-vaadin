1887c7ed601e922b70ea9205239c566b60cecc3b
Chris Henson <chenson42@gmail.com>
2014-11-08 18:27:30 -0500

    dev commit

---
+++ symmetric-ui-common/src/main/java/org/jumpmind/symmetric/ui/common/IItemUpdatedListener.java
@@ -0,0 +1,7 @@
+package org.jumpmind.symmetric.ui.common;
+
+public interface IItemUpdatedListener {
+
+    public void itemUpdated(Object item);
+
+}
---
+++ symmetric-ui-common/src/main/java/org/jumpmind/symmetric/ui/common/UiConstants.java
@@ -0,0 +1,10 @@
+package org.jumpmind.symmetric.ui.common;
+
+final public class UiConstants {
+    private UiConstants() {
+    }
+    
+    public static final String STYLE_BUTTON_LEFT_ALIGNED = "leftAligned";
+
+    public static final int TEXTFIELD_WIDTH = 255;
+}
